cdf38a89a5b5adc08ea4569d382c96e3178afff8
Dima <omeluan.dima@gmail.com>
2021-11-18 18:51:35 +0200

    Spinner was added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 // import sendEmail from './Mailer'
 import {useState} from "react";
+import Spinner from "./Speener";
 
 function App() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [textMessage, setTextMessage] = useState("");
     const [submitBlocked, setSubmitBlocked] = useState(false);
+    const [activeField, setActiveField] = useState(0);
+    const [sendingEmail, setSendingEmail] = useState(false);
 
     let regEmail = /^[a-zA-Z-0-9]+@[a-zA-Z-0-9]+\.[A-Za-z]+$/
 
     const showErrorMessage = (str, errorNumber) => {
-        // 1 - empty string in name or text
-        // 2 - smth wrong with email
+        if(errorNumber === 1) {
+            alert("There is an empty string...");
+        } else {
+            alert("There is something wrong with your email");
+        }
     }
 
     const notEmpty = (str) => {
@@ -35,8 +41,11 @@ function App() {
         if(!submitBlocked) {
             setSubmitBlocked(true);
             if(notEmpty(name) && notEmpty(textMessage) && checkEmail()) {
+                setSendingEmail(true);
                 sendEmail(name, email, textMessage);
-            }
+                // setSendingEmail(false);
+                resetData();
+            } else setSubmitBlocked(false);
         }
     }
 
@@ -45,6 +54,7 @@ function App() {
         setEmail("");
         setTextMessage("");
         setSubmitBlocked(false);
+        setActiveField(0);
     }
 
     const allFieldsEmpty = () => {
@@ -57,39 +67,62 @@ function App() {
 
   //  TODO add database connection (Supabase, or another)
   return (
-    <div>
-        {/*TODO message about a successfully sent data*/}
-        {/*TODO send email with a text*/}
-        {/*TODO add header*/}
-        <input type={"name"} placeholder={"Enter your name"}
-               className={"inputField nameInputField centerBlock"}
-               value={name}
-               onChange={event => setName(event.target.value)}
-        />
-        <input type={"email"} placeholder={"Enter your email"}
-               value={email}
-               onChange={(event => setEmail(event.target.value))}
-               className={"inputField emailInputField centerBlock"} />
-        <input type={"text"} placeholder={"Enter your message here"}
-               value={textMessage}
-               onChange={event => setTextMessage(event.target.value)}
-               className={"inputField textField centerBlock"} />
-        {/*TODO add speener*/}
-        <button className={"centerBlock submitButton"}
-                onClick={submitData}
-                disabled={submitBlocked}
-        >
-            Submit
-        </button>
-        <button className={"centerBlock resetButton"}
-                disabled={allFieldsEmpty()}
-                onClick={resetData}>
-            Reset
-        </button>
-    {/*    TODO add an ability to enter a text (message) - no null*/}
-    </div>
+      <div>
+          { !sendingEmail ?
+          <div>
+              {/*TODO message about a successfully sent data*/}
+              {/*TODO send email with a text*/}
+              {/*TODO add header*/}
+              <input type={"name"} placeholder={"Enter your name"}
+                     className={"inputField nameInputField centerBlock"}
+                     value={name}
+                     onChange={event => setName(event.target.value)}
+                     onFocus={() => setActiveField(1)}
+                     onBlur={() => setActiveField(0)}
+                     style={{
+                         borderBottom: activeField === 1 ? '3px solid #10fc2a' : null
+                     }}
+                  // TODO light-green color if a field is filled
+              />
+              <input type={"email"} placeholder={"Enter your email"}
+                     value={email}
+                     onChange={(event => setEmail(event.target.value))}
+                     onFocus={() => setActiveField(2)}
+                     onBlur={() => setActiveField(0)}
+                     style={{
+                         borderBottom: activeField === 2 ? '3px solid #10fc2a' : null
+                     }}
+                     className={"inputField emailInputField centerBlock"}
+              />
+              {/*TODO red color if a field has error for a 3 seconds*/}
+              <input type={"text"} placeholder={"Enter your message here"}
+                     value={textMessage}
+                     onChange={event => setTextMessage(event.target.value)}
+                     className={"inputField textField centerBlock"}
+                     onFocus={() => setActiveField(3)}
+                     onBlur={() => setActiveField(0)}
+                     style={{
+                         borderBottom: activeField === 3 ? '3px solid #10fc2a' : null
+                     }}
+              />
+              <button className={"centerBlock submitButton"}
+                      onClick={submitData}
+                      disabled={submitBlocked}
+                      style={{
+                          background: submitBlocked ? '#05acbb' : null
+                      }}
+              >
+                  Submit
+              </button>
+              <button className={"centerBlock resetButton"}
+                      disabled={allFieldsEmpty()}
+                      onClick={resetData}>
+                  Reset
+              </button>
+          </div> : Spinner()
+          }
+      </div>
   );
 }
-//TODO add validation for inputs
 //TODO add css files
 export default App;
---
+++ src/Speener.js
@@ -0,0 +1,27 @@
+import './App.css'
+import Loader from 'react-loader-spinner'
+import './App.css'
+import styled from 'styled-components';
+
+function Spinner() {
+    return (
+        <ContainerLoader>
+            <Loader
+                type={"Puff"}
+                color={"#00BFFF"}
+                height={100}
+                width={100}
+                timeout={10 * 3000} // 30 seconds
+            />
+        </ContainerLoader>
+    );
+}
+
+const ContainerLoader = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`
+
+export default Spinner;
